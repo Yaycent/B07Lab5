03fc23747dbc2af9e39832346464826b11f3755b
Anony13531 <raphaelalola135@gmail.com>
2025-10-24 19:19:23 -0400

    Feature circle added

---
+++ circle.java
@@ -0,0 +1,17 @@
+package lab5;
+
+public class circle {
+    Point center;
+    double radius; 
+    
+    public circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+    
+}
+
